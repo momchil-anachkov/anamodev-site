e83895ca4949925da19e6a6d2357e5588730c4bd
momchil-anachkov <momchil.anachkov@gmail.com>
2023-11-22 13:06:18 +0200

    fix footer urls; spruce up portfolio;

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'AnamoDev';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'anamodev';
+export const SITE_DESCRIPTION = 'Making Software Better';
